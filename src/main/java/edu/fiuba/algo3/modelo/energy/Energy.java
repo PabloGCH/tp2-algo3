cb6c26e5eab5535b059a65869dd8ad149907e119
pablogch <pablochoconi@gmail.com>
2023-11-13 00:49:42 -0300

    Methods to fullfill energy classes's contract

--- src/main/java/edu/fiuba/algo3/modelo/energy/Energy.java
+++ src/main/java/edu/fiuba/algo3/modelo/energy/Energy.java
@@ -1,5 +1,18 @@
 package edu.fiuba.algo3.modelo.energy;
 
 public class Energy {
-    
+    private interface EnergyCalculator {
+        Energy calculate(Energy energy);
+    }
+    private interface Points {}
+    private Points points;
+    public Energy calculate(EnergyCalculator calculator) {
+        return this;
+    }
+    public Energy spendEnergyByWineConsumption(int cupsOfWine) {
+        return this;
+    }
+    Energy(Points points) {
+        this.points = points;
+    }
 }
