6e08ea00a0278b993a6354e0b65e9b233adfc34d
pablogch <pablochoconi@gmail.com>
2023-11-13 21:12:06 -0300

    Updated usecase 1

--- src/main/test/edu/fiuba/algo3/entrega_1/UseCase01.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/UseCase01.java
@@ -1,9 +1,5 @@
 package edu.fiuba.algo3.entrega_1;
-
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 
@@ -11,14 +7,23 @@ import edu.fiuba.algo3.modelo.gladiator.Gladiator;
 
 public class UseCase01 {
     @Test
-    public void testGladiatorStartsWithCorrectEnergyAndEquipment() {
+    public void testGladiatorStartsWithCorrectEnergy() {
         //Arrange
-        Gladiator gladiator = new Gladiator();
+        Gladiator gladiator = new Gladiator(); //Should start with 20 energy
         //Act
-        int energyPoints = gladiator.getEnergyAsInteger();
+        Energy energy = gladiator.getEnergy();
+        Points energyPoints = energy.getPoints();
+        int amount = energyPoints.getAmount();
+        //Assert
+        assertTrue(amount == 20);
+    }
+    @Test
+    public void testGladiatorStartsWithNullEquipment() {
+        //Arrange
+        Gladiator gladiator = new Gladiator(); //Should start with 20 energy
+        //Act
         Equipment equipment = gladiator.getEquipment();
         //Assert
-        assumeTrue(equipment instanceof NullEquipment)
-        assumeTrue(energyPoints == 20);
+        assertTrue(equipment instanceof NullEquipment);
     }
 }
