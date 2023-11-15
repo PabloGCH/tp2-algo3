9e31c67a465753e65fec6f1b8ca0f5ead9c32d26
pablogch <pablochoconi@gmail.com>
2023-11-15 18:45:05 -0300

    Add and Substract methods in energy, removed unnecessary Points class

--- src/main/java/edu/fiuba/algo3/modelo/energy/Energy.java
+++ src/main/java/edu/fiuba/algo3/modelo/energy/Energy.java
@@ -1,26 +1,21 @@
 package edu.fiuba.algo3.modelo.energy;
-import edu.fiuba.algo3.modelo.points.Points;
 
 public class Energy {
-    private interface EnergyCalculator {
-        Energy calculate(Energy energy);
+    private int points;
+
+    public Energy add(Energy energy) {
+        return new Energy(this.getPoints() + energy.getPoints());
     }
 
-    private Points points;
-
-    public Energy calculate(EnergyCalculator calculator) {
-        return this;
-    }
-  
-    public Energy spendEnergyByWineConsumption(int cupsOfWine) {
-        return this;
+    public Energy substract(Energy energy) {
+        return new Energy(this.getPoints() - energy.getPoints());
     }
   
     public Energy(int pointsAmount) {
-        this.points = new Points(pointsAmount);
+        this.points = pointsAmount;
     }
 
-    public Points getPoints() {
+    public int getPoints() {
         return this.points;
     }
 }
