1a64cb3beb7a5918b2f608f3ee02c0d11d312067
pablogch <pablochoconi@gmail.com>
2023-11-15 18:59:42 -0300

    Gladiator unit test

---
+++ src/main/test/edu/fiuba/algo3/gladiatorTest/GladiatorTest.java
@@ -0,0 +1,18 @@
+package edu.fiuba.algo3.gladiatorTest;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import edu.fiuba.algo3.modelo.gladiator.Gladiator;
+
+public class GladiatorTest {
+    @Test void energyUpdatedCorrectlyAfterEating() {
+        //Arrange
+        Gladiator gladiator = new Gladiator();
+        //Act
+        gladiator.eat();
+        int energyPoints = gladiator.getEnergy().getPoints();
+        //Assert
+        //Starts with 20 energy, 15 is added after eating
+        assertTrue(energyPoints == 35);
+    }
+}
