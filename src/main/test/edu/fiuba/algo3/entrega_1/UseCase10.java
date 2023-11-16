03303ee257b70c1ebb4f9def2cdd6fa5a4629a37
Geroopaulozzi <gpaulozzi@fi.uba.ar>
2023-11-16 01:02:56 -0300

    use case 10
    Co-authored-by: LucianoWil <lwilberger@fi.uba.ar>
    Co-authored-by: godlegend9 <mbarrionuevof@fi.uba.ar>

--- src/main/test/edu/fiuba/algo3/entrega_1/UseCase10.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/UseCase10.java
@@ -1,5 +1,29 @@
 package edu.fiuba.algo3.entrega_1;
 
+import edu.fiuba.algo3.modelo.gladiator.Gladiator;
+import edu.fiuba.algo3.modelo.squares.Initial;
+import edu.fiuba.algo3.modelo.squares.Square;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UseCase10 {
-    
+    @Test
+    public void testGladiatorWithKeyReceiveNoDamage(){
+        //Arrange
+        Gladiator gladiator = new Gladiator(); //Should start with 20 energy
+        Square initialSquare = new Initial();
+        initialSquare.receiveGladiator(gladiator);
+
+        //Act
+        gladiator.upgrade();
+        gladiator.upgrade();
+        gladiator.upgrade();
+        gladiator.upgrade();
+        gladiator.fightWithBeast();
+        var energy = gladiator.getEnergy();
+        int energyPoints = energy.getPoints();
+        //Assert
+        assertEquals(20, energyPoints);
+    }
 }
