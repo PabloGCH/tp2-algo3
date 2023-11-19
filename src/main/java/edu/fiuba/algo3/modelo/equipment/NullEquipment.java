f49d0abfc849113b4231821f6402c82025e02814
LucianoWil <lwilberger@fi.uba.ar>
2023-11-19 01:04:15 -0300

    Implementation of log system

--- src/main/java/edu/fiuba/algo3/modelo/equipment/NullEquipment.java
+++ src/main/java/edu/fiuba/algo3/modelo/equipment/NullEquipment.java
@@ -1,14 +1,17 @@
 package edu.fiuba.algo3.modelo.equipment;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import edu.fiuba.algo3.modelo.energy.Energy;
 
 public class NullEquipment implements Equipment {
-
+    private static final Logger logger = LogManager.getLogger("vehicles");
     public Equipment upgrade() {
         return new Helmet();
     }
 
     public Energy receiveAttack(Energy energy){
+        logger.info("subtracting energy");
+        logger.info("finished subtracting energy");
         return energy.substract(new Energy(20));
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module edu.fiuba.algo3 {
     requires javafx.controls;
+    requires org.apache.logging.log4j;
     exports edu.fiuba.algo3;
 }
