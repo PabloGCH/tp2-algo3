c11755d6cca60cf4761eaf4d18a81f29351d17af
pablogch <pablochoconi@gmail.com>
2023-11-20 17:45:11 -0300

    usecase-14

--- src/main/test/edu/fiuba/algo3/entrega_2/UseCase14.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/UseCase14.java
@@ -1,5 +1,70 @@
 package edu.fiuba.algo3.entrega_2;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import edu.fiuba.algo3.modelo.mapJsonParser.MapJsonParser;
+
 
 public class UseCase14 {
-    
+    @Test
+    public void validateFoodSquareCreationFromMapJson() {
+        try {
+        var mapParser = new MapJsonParser();
+        var map = mapParser.loadMap(
+            "src/main/resources/files/mapTest.json",
+            "mapTest.json"
+        );
+        } catch(Exception e) {
+            
+        }
+    }
+    @Test
+    public void validateEquipmentUpgradeSquareCreationFromMapJson() {
+        try {
+            var mapParser = new MapJsonParser();
+            var map = mapParser.loadMap(
+                "src/main/resources/files/mapTest.json",
+                "mapTest.json"
+            );
+        } catch(Exception e) {
+
+        }
+    }
+    @Test
+    public void validateInjurySquareCreationFromMapJson() {
+        try {
+            var mapParser = new MapJsonParser();
+            var map = mapParser.loadMap(
+                "src/main/resources/files/mapTest.json",
+                "mapTest.json"
+            );
+        } catch(Exception e) {
+
+        }
+    }
+    @Test
+    public void validateBeastSquareCreationFromMapJson() {
+        try {
+            var mapParser = new MapJsonParser();
+            var map = mapParser.loadMap(
+                "src/main/resources/files/mapTest.json",
+                "mapTest.json"
+            );
+        } catch(Exception e) {
+
+        }
+    }
+    @Test
+    public void validateWineSquareCreationFromMapJson() {
+        try {
+            var mapParser = new MapJsonParser();
+            var map = mapParser.loadMap(
+                "src/main/resources/files/mapTest.json",
+                "mapTest.json"
+            );
+        } catch(Exception e) {
+
+        }
+    }
 }
