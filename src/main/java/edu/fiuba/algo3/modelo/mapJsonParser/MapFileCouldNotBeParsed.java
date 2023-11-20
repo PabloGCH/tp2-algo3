060255fc5a18d336c345192bd6258fa9fa676ec1
pablogch <pablochoconi@gmail.com>
2023-11-20 03:38:36 -0300

    Map json parser

---
+++ src/main/java/edu/fiuba/algo3/modelo/mapJsonParser/MapFileCouldNotBeParsed.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo.mapJsonParser;
+
+public class MapFileCouldNotBeParsed extends Exception{
+    public MapFileCouldNotBeParsed() {
+        super("The map file could not be parsed");
+    }
+}
