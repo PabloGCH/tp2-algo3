ea2c8a38075c0945214f6d832592eb0f7e9011f0
pablogch <pablochoconi@gmail.com>
2023-11-22 19:23:11 -0300

    Map facade hotfix

--- src/main/java/edu/fiuba/algo3/modelo/facade/MapFacade.java
+++ src/main/java/edu/fiuba/algo3/modelo/facade/MapFacade.java
@@ -3,13 +3,14 @@ package edu.fiuba.algo3.modelo.facade;
 import edu.fiuba.algo3.modelo.mapJsonParser.*;
 import edu.fiuba.algo3.modelo.squares.Square;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class MapFacade {
     private String fileSource;
     private String fileName;
     public MapFacade() {
-        this.fileSource = "src\\main\\resources\\files\\map.json";
+        this.fileSource = "src" + File.separator + "main" + File.separator + "resources"+ File.separator + "files" + File.separator + "map.json";
         this.fileName = "map.json";
     }
 
