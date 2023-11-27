5b28de86a037c83cdcd98ee2d3a15bfa93268ebd
LucianoWil <lwilberger@fi.uba.ar>
2023-11-27 18:15:44 -0300

    Applying Factory Method to Square and it´s subclasses

--- src/main/java/edu/fiuba/algo3/modelo/squares/FinishLineFactory.java
+++ src/main/java/edu/fiuba/algo3/modelo/squares/FinishLineFactory.java
@@ -1,7 +1,7 @@
 package edu.fiuba.algo3.modelo.squares;
-/*
-public class FinishLineFactory extends PositionFactory{
-    public Position createPosition(Effect effect){
-        return new FinishLine();
+
+public class FinishLineFactory extends SquareFactory{
+    public Square createSquare(Effect obstacle, Effect prize){
+        return new FinishLine(obstacle, prize);
     }
-}*/
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/squares/SquareFactory.java
@@ -0,0 +1,5 @@
+package edu.fiuba.algo3.modelo.squares;
+
+public abstract class SquareFactory{
+    public abstract Square createSquare(Effect obstacle, Effect prize);
+}
