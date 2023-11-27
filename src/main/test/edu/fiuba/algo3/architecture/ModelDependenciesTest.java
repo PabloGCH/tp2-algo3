32aed1dad9fbd61f48a11f8e39052e43f88f7144
zzz <zpazos@fi.uba.ar>
2023-11-27 14:41:20 -0300

    Fixed model dependencies test
    Model dependencies test updated and passing

--- src/main/test/edu/fiuba/algo3/architecture/ModelDependenciesTest.java
+++ src/main/test/edu/fiuba/algo3/architecture/ModelDependenciesTest.java
@@ -11,12 +11,14 @@ public class ModelDependenciesTest {
     private final String MODEL = "..modelo..";
     private final String JAVA_LANG = "java..";
     private final String JUNIT = "org.junit..";
+    private final String LOG4J = "org.apache.logging.log4j..";
+    private final String JSON = "org.json.simple..";
 
     @Test
     public void elModeloSoloPuedeReferenciarClasesDelModeloAdemasJavaAdemasJunit() {
         JavaClasses importedClasses = new ClassFileImporter().importPackages("edu.fiuba.algo3.modelo");
 
-        String[] listOfPackages = {MODEL, JAVA_LANG, JUNIT};
+        String[] listOfPackages = {MODEL, JAVA_LANG, JUNIT, LOG4J, JSON};
 
         ArchRule myRule = classes().that().resideInAPackage(MODEL)
                 .should().onlyDependOnClassesThat().resideInAnyPackage(listOfPackages);
