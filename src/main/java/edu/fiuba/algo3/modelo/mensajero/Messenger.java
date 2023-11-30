af25737387a40d24137a7db01dca5d109b45a65c
zzz <zpazos@fi.uba.ar>
2023-11-30 01:10:27 -0300

    Deleted LoggerMessages interface

--- src/main/java/edu/fiuba/algo3/modelo/mensajero/Messenger.java
+++ src/main/java/edu/fiuba/algo3/modelo/mensajero/Messenger.java
@@ -1,36 +1,25 @@
 package edu.fiuba.algo3.modelo.mensajero;
 import org.apache.logging.log4j.Logger;
 
-public class Messenger implements LoggerMessages{
+public class Messenger {
     private Logger logger;
 
     public Messenger(Logger newLogger)
     {
         this.logger = newLogger;
     }
-
-    //public void assignLogger(Logger newLogger){
-        ;
-    //}
-
-    @Override
     public void info(String message) throws Exception {
         unassignedLogger();
         logger.info(message);
     }
-
-    @Override
     public void error(String message) throws Exception {
         unassignedLogger();
         logger.error(message);
     }
-
-    @Override
     public void warn(String message) throws Exception {
         unassignedLogger();
         logger.warn(message);
     }
-
     private void unassignedLogger() throws Exception {
         if(logger == null){
             throw new Exception("Unassigned logger");
