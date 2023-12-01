697bd2733f07f4cc39896d1c16cadd4eeffa7cde
LucianoWil <lwilberger@fi.uba.ar>
2023-12-01 19:34:36 -0300

    Delete Resources enums

--- src/main/java/edu/fiuba/algo3/controller/SquareController.java
+++ src/main/java/edu/fiuba/algo3/controller/SquareController.java
@@ -1,9 +1,9 @@
 package edu.fiuba.algo3.controller;
-import edu.fiuba.algo3.modelo.Resources;
 import edu.fiuba.algo3.modelo.squares.Position;
 import javafx.scene.layout.*;
 
 public class SquareController {
+    private final String SQUARE_STYLE_PATH = "/styles/square.css";
     private Position square;
     public SquareController( Position square ) {
         this.square = square;
@@ -11,7 +11,7 @@ public class SquareController {
 
     public Pane draw() {
         Pane squarePane = new Pane();
-        squarePane.getStylesheets().add(getClass().getResource(Resources.SQUARE_STYLE_PATH.getValue()).toExternalForm());
+        squarePane.getStylesheets().add(getClass().getResource(SQUARE_STYLE_PATH).toExternalForm());
         squarePane.getStyleClass().add("square");
         return squarePane;
     }
