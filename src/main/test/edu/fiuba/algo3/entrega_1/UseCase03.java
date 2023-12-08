068dab9bd293ecae6eb088d7e60490226b39a72d
LucianoWil <lwilberger@fi.uba.ar>
2023-12-08 03:33:20 -0300

    Rebase fixes

--- src/main/test/edu/fiuba/algo3/entrega_1/UseCase03.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/UseCase03.java
@@ -1,6 +1,5 @@
 package edu.fiuba.algo3.entrega_1;
 
-import edu.fiuba.algo3.modelo.squares.Position;
 import edu.fiuba.algo3.modelo.factories.*;
 import edu.fiuba.algo3.modelo.gladiator.Gladiator;
 import edu.fiuba.algo3.modelo.position.Position;
