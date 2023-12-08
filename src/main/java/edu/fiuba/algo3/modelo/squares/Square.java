63f749edbc7c81aadc35b70299a439eb677c81c2
LucianoWil <lwilberger@fi.uba.ar>
2023-12-08 02:24:32 -0300

    merge

--- src/main/java/edu/fiuba/algo3/modelo/squares/Square.java
+++ src/main/java/edu/fiuba/algo3/modelo/squares/Square.java
@@ -1,6 +1,5 @@
 package edu.fiuba.algo3.modelo.squares;
 
-import edu.fiuba.algo3.modelo.Position;
 import edu.fiuba.algo3.modelo.gladiator.Gladiator;
 import edu.fiuba.algo3.modelo.position.Position;
 
